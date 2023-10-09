c9dc89a767d73a4cb8af0ccb8dd7e996c009a850
Rafael Nagatomo <rafael_nagatomo@hotmail.com>
2023-10-10 08:00:35 +0900

    crud edit delete

--- js/release.js
+++ js/release.js
@@ -22,34 +22,25 @@ const chkRadioBtn = () => {
 // Insere uma linha no CRUD com os valores do formulario
 
 function insertItem(id) {
-  let tr = document.createElement('tr')
 
-  tr.innerHTML = `
-  <td>${lDate.value}</td>
-  <td>${op}</td>
-  <td>${lDescription.value}</td>
-  <td>${lType.value}</td>
-  <td>${lCategory.value}</td>
-  <td>R$${lValue.value}</td>
-  <td>
-  <i onclick="editItem(${id})" id="i-edit" class="material-symbols-outlined">edit_note</i>
-      <i onclick="editItem(${id})" id="i-del" class="material-symbols-outlined">delete</i>
-      </td>
-      `
-  tbody.appendChild(tr)
-
-
+  tbody.insertAdjacentHTML("afterbegin",
+    `<tr>
+        <td>${lDate.value}</td>
+        <td>${op}</td>
+        <td>${lDescription.value}</td>
+        <td>${lType.value}</td>
+        <td>${lCategory.value}</td>
+        <td>R$${lValue.value}</td>
+        <td>
+          <i onclick="editItem(${id})" id="i-edit" class="material-symbols-outlined">edit_note</i>
+          <i onclick="editItem(${id})" id="i-del" class="material-symbols-outlined">delete</i>
+        </td>
+      </tr>`)
 }
-
 
 //Checa botao radio, insere linha no CRUD, apaga valores do formulario
 
 btnInput.onclick = e => {
-
-  let data = []
-  let item = { id: data.length + 1, Date: lDate.value, Description: lDescription.value, Type: lType.value, Category: lCategory.value, Value: lValue.value }
-
-  data.push = (item)
 
   chkRadioBtn();
   insertItem();
---
+++ js/teste.js
@@ -0,0 +1,8 @@
+
+let data = []
+const item = {}
+let y = new item ({ 'id': data.length + 1, 'Date': 'lDate.value', 'Description': 'lDescription.value', 'Type': 'lType.value', 'Category': 'lCategory.value', 'Value': 'lValue.value' })
+let x = new item ({ 'id': data.length + 1, 'Date': '20252', 'Description': 'agerger', 'Type': 'teste', 'Category': 'rvalue', 'Value': '20200' })
+
+
+console.log(data)
